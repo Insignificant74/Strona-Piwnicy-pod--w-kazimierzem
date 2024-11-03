e0944ec464d15c0bbe0951b9127c8cc7cb343c25
Wiktor Kunicki <kunickiwiktor74@gmail.com>
2024-11-03 20:56:09 +0100

    temp 2

--- strona.js
+++ strona.js
@@ -1,12 +1,21 @@
 window.onload = function () {
   const mainButton = document.getElementById("mainButton");
   const foodButton = document.getElementById("foodButton");
+  const foodDropdown = foodButton.getElementsByClassName("dropdown");
+  const foodMenu = document.getElementById("foodMenu");
+  const wineMenu = document.getElementById("wineMenu");
   const contactButton = document.getElementById("contactButton");
 
   mainButton.addEventListener("click", function () {
     window.location.href=("StronaGlowna.html");
   });
-  foodButton.addEventListener("click", function () {
+  foodButton.addEventListener("mouseover", function () {
+    foodDropdown.style.display="block";
+  });
+  foodButton.addEventListener("mouseoff", function () {
+    foodDropdown.style.display="none";
+  });
+  foodMenu.addEventListener("click", function () {
     window.location.href=("StronaMenu.html");
   });
   contactButton.addEventListener("click", function () {
